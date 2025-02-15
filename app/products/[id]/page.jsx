cfc0e48ac80c7a28672170a5f280477f44f7f5e8
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-02-15 23:37:12 +0600

    refactor product detail page to use Next.js Image component for optimized image loading and layout improvements

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,4 +1,6 @@
+
 import axios from "axios";
+import Image from "next/image";
 
 const page = async ({ params }) => {
 
@@ -8,13 +10,28 @@ const page = async ({ params }) => {
     const { data } = await axios(`${process.env.LIVE_LINK}/api/products/${id?.id}`)
 
     console.log(data);
-    const { name, images , price , currency} = data || {}
+    const { name, images, price, currency } = data || {}
+
     return (
-        <div className="grid grid-cols-2 gap-5 mt-5">
-            <img src={images[0]?.url} alt="" />
-            <div className="">
-                <p className="text-2xl font-semibold">{name}</p>
-                <p>{currency} {price}</p>
+        <div className="w-10/12 mx-auto">
+            <div className="grid md:grid-cols-2 gap-5 mt-5">
+                <Image
+                    src={images[0]?.url}
+                    alt="Product Image"
+                    loading="lazy"
+                    sizes="100vw"
+                    style={{
+                        width: '100%',
+                        height: 'auto',
+                    }}
+                    width={500}
+                    height={300}
+                    className="object-cover"
+                />
+                <div className="">
+                    <p className="text-2xl font-semibold">{name}</p>
+                    <p>{currency} {price}</p>
+                </div>
             </div>
         </div>
     )
